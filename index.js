4434907afc04023c5611c09228445460e0b73b6f
Haseeb Mustafa <66658449+Haseeb83@users.noreply.github.com>
2024-04-24 12:39:46 +0500

    commit changes in main index.ts file

--- index.js
+++ index.js
@@ -2,15 +2,13 @@
 import inquirer from "inquirer";
 let myBalance = 10000;
 let mypin = 1234;
-let pinanswer = await inquirer.prompt(
-    [
+let pinanswer = await inquirer.prompt([
     {
-        name:"pin",
-        message:"Enter your Pin",
-        type:"number",
+        name: "pin",
+        message: "Enter your Pin",
+        type: "number",
     }
-]
-);
+]);
 if (pinanswer.pin === mypin) {
     console.log("Correct pin code!!");
     let operationsanswer = await inquirer.prompt([
@@ -30,13 +28,13 @@ if (pinanswer.pin === mypin) {
                 type: "number",
             }
         ]);
-
         myBalance -= amountanswer.amount;
         console.log("Your current remaining balance is: " + myBalance);
-
-    } else if (operationsanswer.operations.toLowerCase() === "check balance") {
+    }
+    else if (operationsanswer.operations.toLowerCase() === "check balance") {
         console.log("Your balance is: " + myBalance);
     }
-} else {
+}
+else {
     console.log("Incorrect pin number");
 }
